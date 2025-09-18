95b93ef98df553a2b04087e7cf970a7c87eb91b9
KANYAN3 <kanyan3@nationwide.com>
2025-09-18 19:32:49 -0400

    FirstCommit

--- pages/login.page.ts
+++ pages/login.page.ts
@@ -19,8 +19,17 @@ export class Login {
     }
 
     public async loginAsUser(userName: string) {
-        await this.page.locator(this.userNameField).fill(userName)
+        await this.page.locator(this.userNameField).fill('Priyanka')
         await this.page.locator(this.passwordField).fill(this.password)
         await this.page.locator(this.loginButton).click()
+    }
+
+    public async validateErrorMessage() {
+        const errorMessage = this.page.locator('h3[data-test="error"]')
+        await errorMessage.waitFor({ state: 'visible', timeout: 5000 })
+        const errorText = await errorMessage.textContent()
+        if (errorText?.trim() !== 'Epic sadface: Username and password do not match any user in this service') {
+            throw new Error(`Expected error message to be 'Epic sadface: Sorry, this user has been locked out. '${errorText}'`);
+        }
     }
 }
